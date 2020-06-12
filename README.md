63b7e96e576867ac533eb0fa6e5ae38ca82acd80
Blake Tarter <blakertarter@gmail.com>
2020-06-12 10:23:19 -0500

    Add basic subscription

---
+++ README.md
@@ -0,0 +1,18 @@
+# GraphQL Subscriptions
+
+## Steps
+
+### Server
+
+- Set up GraphQL server
+
+## Resources
+
+### Server
+
+- https://www.apollographql.com/docs/graphql-subscriptions
+- https://www.apollographql.com/docs/apollo-server/data/subscriptions/
+
+### Client
+
+- https://www.apollographql.com/docs/react/data/subscriptions/
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
 import { ApolloServer, gql } from 'apollo-server';
+import { PubSub } from 'graphql-subscriptions';
+
+export const pubsub = new PubSub();
 
 const typeDefs = gql`
   type Message {
-    author: String
-    body: String
+    author: String!
+    body: String!
+  }
+
+  type Subscription {
+    messageAdded: Message!
   }
 
   type Query {
     messages: [Message!]!
   }
+
+  type Mutation {
+    addMessage(author: String!, body: String!): Message!
+  }
 `;
 
-const messages = [
+interface Message {
+  author: string
+  body: string
+}
+
+const messages: Message[] = [
   {
     author: 'Jolby Johnson',
     body: 'Hey',
@@ -22,14 +38,34 @@ const messages = [
   },
 ];
 
+const MESSAGE_ADDED = 'MESSAGE_ADDED'
+
 const resolvers = {
-  Query: {
-    messages: () => messages,
+  Subscription: {
+    messageAdded: {
+      subscribe() {
+        return pubsub.asyncIterator([MESSAGE_ADDED])
+      },
+    }
   },
+  Query: {
+    messages() {
+      return messages
+    },
+  },
+  Mutation: {
+    addMessage(_root: unknown, args: Message, _context: unknown) {
+      messages.push(args)
+      pubsub.publish(MESSAGE_ADDED, { messageAdded: args })
+
+      return args
+    }
+  }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
+server.listen().then(({ url, subscriptionsUrl }) => {
   console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`);
 });
